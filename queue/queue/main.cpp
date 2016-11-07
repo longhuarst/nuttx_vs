2eb62b9f43c0c7b441d819c8cc67697372ff812b
longhuarst <841105197@qq.com>
2016-11-07 14:52:53 +0800

    add sq dq queue

---
+++ queue/queue/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdlib.h>
+#include "queue.h"
+
+
+
+
+int main(int argc, char *argv[])
+{
+	sq_queue_t *sqlist = new sq_queue_t;
+
+
+	
+
+	sq_init(sqlist);
+
+
+
+
+
+
+	system("pause");
+
+
+}
